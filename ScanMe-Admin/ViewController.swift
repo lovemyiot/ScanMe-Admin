daa7f564383cdab9c4a491ac8fa1cd4a053ff7d3
Jacek Kopaczel <jacek.kopaczel@accenture.com>
2021-08-27 18:20:59 +0200

    Initial Commit

---
+++ ScanMe-Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScanMe-Admin
+//
+//  Created by jacek.kopaczel on 27/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
